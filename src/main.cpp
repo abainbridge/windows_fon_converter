c5af1eae9664a9eb1b1c49ce4452d24d2d53f41d
Andrew Bainbridge <2005@deadfrog.co.uk>
2020-06-20 08:39:42 +0100

    Replaced BinaryFileReader to plain old FILE because it FILE lets me fread into existing memory, which I need to do. BinaryFileReader was meant to make the code simpler, but it is actually making it harder.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 
+
 // First 0x40 bytes of file.
 struct OldExeHeader {
     u8 id[2];                       // "MZ"
@@ -113,98 +114,71 @@ struct _Glyph {
     u16 bitmap_offset;
 };
 
-struct Fnt {
-
-//    struct FontDirEntry {
-        u16 version;                    // 0x200
-        u16 size[2];                    // 0xf76
-        char copyright[60];
-        u16 type;                       // 0 - Bit 0 is zero, is therefore raster not vector.
-        u16 point_size;                 // 0xa
-        u16 vert_dpi;                   // 0x60
-        u16 hori_dpi;                   // 0x60
-        u16 ascent;                     // 0xb - Num pixels from top of glyph to baseline. Useful for aligning baseline of fonts of different heights.
-        u16 internal_leading;           // 0
-        u16 external_leading;           // 0
-        u8 italic;                      // 0
-        u8 underline;                   // 0
-        u8 strikeout;                   // 0
-        u16 weight;                     // 0x190 - 100=light, 200=regular, 300=semi-bold
-        u8 char_set;                    // 0
-        u16 pix_width;                  // 7 - Pixel width of glyph. 0=variable.
-        u16 pix_height;                 // 13
-        u8 pitch_and_family;            // 0x30 - b0=0=fixed width, 
-        u16 avg_width;                  // 7
-        u16 max_width;                  // 7
-        u8 first_char;                  // 0x20
-        u8 last_char;                   // 0xff
-        u8 default_char;                // 0xdf
-        u8 break_char;                  // 0
-        u16 width_bytes;                // 0x627 - Number of bytes in each row of the bitmap. Must be even.
-        u32 device;                     // 0
-        u32 name_offset;                // 0xf6b - Offset from start of this struct for font name.
-        u32 pad0;                       // 0
-        u32 bitmap_offset;              // 0x3fe
-        u8 pad1;
+struct FntHeader {
+    u16 version;                    // 0x200
+    u16 size[2];                    // 0xf76
+    char copyright[60];
+    u16 type;                       // 0 - Bit 0 is zero, is therefore raster not vector.
+    u16 point_size;                 // 0xa
+    u16 vert_dpi;                   // 0x60
+    u16 hori_dpi;                   // 0x60
+    u16 ascent;                     // 0xb - Num pixels from top of glyph to baseline. Useful for aligning baseline of fonts of different heights.
+    u16 internal_leading;           // 0
+    u16 external_leading;           // 0
+    u8 italic;                      // 0
+    u8 underline;                   // 0
+    u8 strikeout;                   // 0
+    u16 weight;                     // 0x190 - 100=light, 200=regular, 300=semi-bold
+    u8 char_set;                    // 0
+    u16 pix_width;                  // 7 - Pixel width of glyph. 0=variable.
+    u16 pix_height;                 // 13
+    u8 pitch_and_family;            // 0x30 - b0=0=fixed width, 
+    u16 avg_width;                  // 7
+    u16 max_width;                  // 7
+    u8 first_char;                  // 0x20
+    u8 last_char;                   // 0xff
+    u8 default_char;                // 0xdf
+    u8 break_char;                  // 0
+    u16 width_bytes;                // 0x627 - Number of bytes in each row of the bitmap. Must be even.
+    u32 device;                     // 0
+    u32 name_offset;                // 0xf6b - Offset from start of this struct for font name.
+    u32 pad0;                       // 0
+    u32 bitmap_offset;              // 0x3fe
+    u8 pad1;
 //        u32 flags;                      // 1=fixed, 2=proportional, 16=1 colour
 //        u16 abc_spaces[3];
 //        u32 colour_offset;
 //        u8 pad2[2];
-        _Glyph glyph_table[1];           // Num entries is last_char - first_char + 2.
-//         char device_name[];
-//         char face_name[];
-//    };
 };
 #pragma pack(pop)
 
 
-struct BinaryFileReader {
-    char *data;
-    int size;
-    int offset;
-
-    bool ReadEntireFile(char const *path) {
-        FILE *f = fopen(path, "rb");
-        if (!f) return false;
-        if (fseek(f, 0, SEEK_END) != 0) goto err;
-        int size = ftell(f);
-        if (fseek(f, 0, SEEK_SET) != 0) goto err;
-        data = new char[size];
-        if (fread(data, 1, size, f) != size) goto err;
-        fclose(f);
-
-        offset = 0;
-        return true;
-
-    err:
-        fclose(f);
-        return false;
-    }
-
-    char *Read(int size) {
-        char *rv = data + offset;
-        offset += size;
-        return rv;
-    }
+struct FullFnt {
+    FntHeader hdr;
+    _Glyph glyph_table[1];           // Num entries is last_char - first_char + 2.
+    char *name;
 };
 
+
 // FNT resource format explanation https://jeffpar.github.io/kbarchive/kb/065/Q65123/
-void ReadFntResourceItem(BinaryFileReader &f, int block_size) {
-    //printf("Resource table item offset 0x%x\n", f.offset);
-    ResourceTableItem *rt_item = (ResourceTableItem *)f.Read(sizeof(ResourceTableItem));
-    assert(rt_item->resource_id & 0x8000);
-    //printf("Item id %i\n", rt_item->resource_id);
+FullFnt *ReadFntResourceItem(FILE *f, int block_size) {  
+    ResourceTableItem rt_item; fread(&rt_item, 1, sizeof(ResourceTableItem), f);
+    ReleaseAssert(rt_item.resource_id & 0x8000, "Bad resource id");
+    
+    int next_item_offset = ftell(f);
 
-    int next_item_offset = f.offset;
-
-    int fnt_data_offset = block_size * rt_item->data_offset;
+    int fnt_data_offset = block_size * rt_item.data_offset;
     //printf("FNT data offset is 0x%x\n", fnt_data_offset);
 
-    f.offset = fnt_data_offset;
-    Fnt *fnt = (Fnt *)f.Read(sizeof(Fnt));
+    fseek(f, fnt_data_offset, SEEK_SET);
+    
+    FullFnt full_fnt;
+    FntHeader *fnt = &full_fnt.hdr;
+    fread(fnt, 1, sizeof(FntHeader), f);
 
     ReleaseAssert(fnt->version == 0x200, "Version 0x%x found. Only version 0x200 supported.",
         fnt->version);
+    
     //printf("Name: %s\n", (char *)fnt + fnt->name_offset);
     //printf("%s\n", fnt->copyright);
     //printf("Bitmap offset is 0x%x\n", fnt->bitmap_offset + fnt_data_offset);
@@ -214,8 +188,9 @@ void ReadFntResourceItem(BinaryFileReader &f, int block_size) {
     int bmp_num_bytes = bytes_per_row * fnt->pix_height;
     //printf("Bitmap num bytes is 0x%x\n", bmp_num_bytes);
 
-    f.offset = fnt->bitmap_offset + fnt_data_offset;
-    u8 *bmp = (u8 *)f.Read(bmp_num_bytes);
+    fseek(f, fnt->bitmap_offset + fnt_data_offset, SEEK_SET);
+    u8 *bmp = new u8[bmp_num_bytes];
+    fread(bmp, 1, bmp_num_bytes, f);
 
     int num_chars = fnt->last_char - fnt->first_char + 1;
     int num_columns = (fnt->max_width + 7) / 8;
@@ -239,7 +214,9 @@ void ReadFntResourceItem(BinaryFileReader &f, int block_size) {
         }
     }
 
-    f.offset = next_item_offset;
+    fseek(f, next_item_offset, SEEK_SET);
+
+    return NULL;
 }
 
 
@@ -247,72 +224,70 @@ int main() {
     CreateWin(800, 600, WT_WINDOWED, ".FON Converter");
     BitmapClear(g_window->bmp, g_colourBlack);
 
-    BinaryFileReader f;
     char const *path = "h:/arcs/fonts/myfonts/trowel_variable2.fon";
     //char const *path = "h:/arcs/fonts/coure.fon";
-    if (!f.ReadEntireFile(path)) {
-        return -1;
-    }
+    FILE *f = fopen(path, "rb");
+    ReleaseAssert(f, "Couldn't open file '%s'", path);
 
-    OldExeHeader *old_hdr = (OldExeHeader *)f.Read(sizeof(OldExeHeader));
+    OldExeHeader old_hdr; fread(&old_hdr, 1, sizeof(OldExeHeader), f);
     //printf("Read old header of size %i\n", sizeof(OldExeHeader));
     
-    int new_hdr_offset = old_hdr->num_paragraphs_in_header * 16 + sizeof(OldExeHeader);
+    int new_hdr_offset = old_hdr.num_paragraphs_in_header * 16 + sizeof(OldExeHeader);
     //printf("New header spans 0x%x to 0x%x.\n", new_hdr_offset, new_hdr_offset + sizeof(NewExeHeader) - 1);
 
-    f.offset = new_hdr_offset;
-    NewExeHeader *new_hdr = (NewExeHeader *)f.Read(sizeof(NewExeHeader));
-    //printf("New header ID is %c%c\n", new_hdr->id[0], new_hdr->id[1]);
+    fseek(f, new_hdr_offset, SEEK_SET);
+    NewExeHeader new_hdr; fread(&new_hdr, 1, sizeof(NewExeHeader), f);
+    //printf("New header ID is %c%c\n", new_hdr.id[0], new_hdr.id[1]);
     
-    int seg_table_offset = new_hdr_offset + new_hdr->seg_table_offset;
+    int seg_table_offset = new_hdr_offset + new_hdr.seg_table_offset;
     //printf("Seg table spans 0x%x to 0x%x\n", seg_table_offset,
-//        seg_table_offset + new_hdr->seg_table_num_items * 8 - 1);
+//        seg_table_offset + new_hdr.seg_table_num_items * 8 - 1);
 
-    int res_table_offset = new_hdr_offset + new_hdr->res_table_offset;
-    int res_table_num_blocks = new_hdr->num_resource_entries / sizeof(ResourceTableBlock);
+    int res_table_offset = new_hdr_offset + new_hdr.res_table_offset;
+    int res_table_num_blocks = new_hdr.num_resource_entries / sizeof(ResourceTableBlock);
     //printf("Resource table num blocks is %i\n", res_table_num_blocks);
-    //printf("Resource table spans 0x%x to 0x%x\n", res_table_offset, new_hdr->num_resource_entries * sizeof(ResourceTableBlock));
+    //printf("Resource table spans 0x%x to 0x%x\n", res_table_offset, new_hdr.num_resource_entries * sizeof(ResourceTableBlock));
 
-    //printf("Resident name table offset is 0x%x\n", new_hdr->resi_name_table_offset + new_hdr_offset);
-    //printf("Module ref table offset is 0x%x\n", new_hdr->module_ref_table_offset + new_hdr_offset);
-    //printf("Imported name table offset is 0x%x\n", new_hdr->imported_names_table_offset + new_hdr_offset);
-    //printf("Non resi name table offset 0x%x. Num items %i\n", new_hdr->non_resi_name_table_offset,
-//        new_hdr->non_resi_name_table_offset + new_hdr->non_resi_name_table_num_bytes);
+    //printf("Resident name table offset is 0x%x\n", new_hdr.resi_name_table_offset + new_hdr_offset);
+    //printf("Module ref table offset is 0x%x\n", new_hdr.module_ref_table_offset + new_hdr_offset);
+    //printf("Imported name table offset is 0x%x\n", new_hdr.imported_names_table_offset + new_hdr_offset);
+    //printf("Non resi name table offset 0x%x. Num items %i\n", new_hdr.non_resi_name_table_offset,
+//        new_hdr.non_resi_name_table_offset + new_hdr.non_resi_name_table_num_bytes);
     
-    f.offset = res_table_offset;
-    char *resource_table = f.data + f.offset;
-    u16 *alignment_shift_amount = (u16*)f.Read(sizeof(u16));
-    int block_size = 1 << *alignment_shift_amount;
+    fseek(f, res_table_offset, SEEK_SET);
+//    char *resource_table = f.data + f.offset;
+    u16 alignment_shift_amount; fread(&alignment_shift_amount, 1, sizeof(u16), f);
+    int block_size = 1 << alignment_shift_amount;
 
     while (1) {
         //printf("\nResource block offset 0x%x. ", f.offset);
-        ResourceTableBlock *rtblock = (ResourceTableBlock *)f.Read(sizeof(ResourceTableBlock));
-        //printf("type id 0x%x. Num of this type %i\n", rtblock->type_id, rtblock->num_of_this_type);
+        ResourceTableBlock rtblock; fread(&rtblock, 1, sizeof(ResourceTableBlock), f);
+        //printf("type id 0x%x. Num of this type %i\n", rtblock.type_id, rtblock.num_of_this_type);
 
-        if (rtblock->type_id == 0) {
+        if (rtblock.type_id == 0) {
             break;
         }
-        else if (rtblock->type_id == 0x8008) {
-            int next_block_offset = f.offset + sizeof(ResourceTableItem)* rtblock->num_of_this_type;
+        else if (rtblock.type_id == 0x8008) {
+            int next_block_offset = ftell(f) + sizeof(ResourceTableItem)* rtblock.num_of_this_type;
 
-            for (int i = 0; i < 1; i++) {
+            for (int i = 0; i < 4; i++) {
                 BitmapClear(g_window->bmp, g_colourBlack);
-//            for (int i = 0; i < rtblock->num_of_this_type; i++) {
+//            for (int i = 0; i < rtblock.num_of_this_type; i++) {
                 ReadFntResourceItem(f, block_size);
             }
 
             break;
         }
         else {
-            int next_block_offset = f.offset + sizeof(ResourceTableItem) * rtblock->num_of_this_type;
+            int next_block_offset = ftell(f) + sizeof(ResourceTableItem) * rtblock.num_of_this_type;
             
-            ResourceTableItem *rt_item = (ResourceTableItem *)f.Read(sizeof(ResourceTableItem));
-            if (rtblock->type_id == 0x8007) {
-                assert(!(rt_item->resource_id & 0x8000));
+            ResourceTableItem rt_item; fread(&rt_item, 1, sizeof(ResourceTableItem), f);
+            if (rtblock.type_id == 0x8007) {
+                ReleaseAssert(!(rt_item.resource_id & 0x8000), "Bad resource id 2");
                 //printf("Font dir name %s\n", resource_table + rt_item->resource_id);
             }
     
-            f.offset = next_block_offset;
+            fseek(f, next_block_offset, SEEK_SET);
         }
     }
 
